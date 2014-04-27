7ac24c25b6862c81652a897ea1c7fe6fbc7633c3
Xciles <xciles@gmail.com>
2014-04-27 17:45:40 +0200

    Changing AssemblyInfos

--- PclValueInjecter.Tests/Properties/AssemblyInfo.cs
+++ PclValueInjecter.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PclValueInjecter.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Hans Harts")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- PclValueInjecter.iOS.Tests/Properties/AssemblyInfo.cs
+++ PclValueInjecter.iOS.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PclValueInjecter.iOS.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Hans Harts")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
