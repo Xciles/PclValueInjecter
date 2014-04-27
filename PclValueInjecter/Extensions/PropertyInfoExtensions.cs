7c249cc6e810e5c63edbf194a0d744ed0f29f3de
Xciles <xciles@gmail.com>
2014-04-27 13:59:59 +0200

    Forgot to add these 2 ;)

---
+++ PclValueInjecter/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Xciles.PclValueInjecter.Extensions
+{
+    public static class PropertyInfoExtensions
+    {
+        public static object GetValue(this PropertyInfo propertyInfo, object obj)
+        {
+            return propertyInfo.GetValue(obj, null);
+        }
+
+        public static void SetValue(this PropertyInfo propertyInfo, object obj, object value)
+        {
+            propertyInfo.SetValue(obj, value, null);
+        }
+    }
+}
---
+++ PclValueInjecter/Extensions/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Xciles.PclValueInjecter.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static Boolean IsAnonymousType(this Type type)
+        {
+            var hasCompilerGeneratedAttribute = type.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false).Length > 0;
+            var nameContainsAnonymousType = type.FullName.Contains("AnonymousType");
+            var isAnonymousType = hasCompilerGeneratedAttribute && nameContainsAnonymousType;
+
+            return isAnonymousType;
+        }
+    }
+}
