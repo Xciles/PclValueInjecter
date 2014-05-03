f6dd3305dee9ed0d1e456c36396a20d895dfd8fa
Xciles <xciles@gmail.com>
2014-05-03 18:47:42 +0200

    Changing info for NuGet

--- PclValueInjecter/Properties/AssemblyInfo.cs
+++ PclValueInjecter/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PclValueInjecter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xciles.PclValueInjecter")]
+[assembly: AssemblyDescription("A PclValueInjecter based on ValueInjecter but now for PCL! This library is still under development!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PclValueInjecter")]
+[assembly: AssemblyProduct("Xciles.PclValueInjecter")]
 [assembly: AssemblyCopyright("Copyright © Hans Harts")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,4 +25,3 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
